1c00d0faec060e92e3d792ce9c0ba75e43b06c9e
kurt2019 <artemkurt@ukr.net>
2023-09-01 20:50:57 +0300

    finish Testimony.tsx

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import styled from "styled-components";
+import {FlexWrapper} from "../FlexWrapper";
 
 export const Slider = () => {
     return (
         <StyledSlider>
-            <Slide>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</Text>
-                <Name>@ivan ivanow</Name>
-            </Slide>
+            <FlexWrapper>
+                <Slide>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.</Text>
+                    <Name>@ivan ivanow</Name>
+                </Slide>
+            </FlexWrapper>
+            <Pagination>
+                <span> </span>
+                <span> </span>
+                <span> </span>
+            </Pagination>
         </StyledSlider>
     );
 };
 
 const StyledSlider = styled.div`
-    border: 1px solid red;
-    max-width: 500px;
+  border: 1px solid red;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 const Slide = styled.div`
@@ -27,4 +40,14 @@ const Text = styled.p`
 
 const Name = styled.span`
     
+`
+
+const Pagination = styled.span`
+    span {
+      display: inline-block;
+      width: 10px;
+      height: 10px;
+      margin: 5px;
+      background-color: deeppink;
+    }
 `
