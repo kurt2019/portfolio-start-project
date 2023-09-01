fb3bde5e1cfcbfa2f1dcbe86bc394f3f316dbe71
kurt2019 <artemkurt@ukr.net>
2023-09-01 12:37:09 +0300

    Reused the Menu.tsx component in Works.tsx

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -10,21 +10,16 @@ export const Menu = (props: {menuItems: Array<string> }) => {
     return (
         <StyledMenu>
             <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/">Skills</a>
-                </li>
-                <li>
-                    <a href="/">Works</a>
-                </li>
-                <li>
-                    <a href="/">Testimony</a>
-                </li>
-                <li>
-                    <a href="/">Contact</a>
-                </li>
+
+                {props.menuItems.map((item , index) => {
+                    return (
+                        <li key={index}>
+                            <a href="" >{item}</a>
+                        </li>
+                    )
+                })}
+
+
             </ul>
         </StyledMenu>
     );
--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
+import {Menu} from "../../../components/menu/Menu";
+
+const worksItems = ["All", "landing page", "React", "spa"]
 
 export const Works = () => {
     return (
         <StyledWorks>
             <SectionTitle>My Works</SectionTitle>
-
+            <Menu menuItems={worksItems}/>
         </StyledWorks>
     );
 };
