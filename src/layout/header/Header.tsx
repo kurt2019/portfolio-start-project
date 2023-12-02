9b5c8f9631435c844ad5b5f4b979f8842f2aad33
kurt2019 <artemkurt@ukr.net>
2023-12-02 04:37:34 +0200

    finish Header 2

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 
 const items =["Home", "Skills", "Works", "Testimony", "Contact"]
@@ -14,7 +14,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
             <Logo/>
-            <Menu menuItems={items}/>
+            <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
