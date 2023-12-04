66b2c5ce3a817c53e09e660d1dd7911806a14c0c
kurt2019 <artemkurt@ukr.net>
2023-12-04 15:45:16 +0200

    finish Skills(part 2)

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Icon} from "../../../../components/icon/Icon";
 import styled from "styled-components";
+import {FlexWrapper} from "../../../../components/FlexWrapper";
 
 type SkillPropsType = {
     iconId: string
@@ -11,23 +12,52 @@ type SkillPropsType = {
 export const Skill = (props: SkillPropsType) => {
     return (
         <StyledSkill>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
             <Icon iconId={props.iconId} />
+                </IconWrapper>
             <SkillTitle>{props.title}</SkillTitle>
             <Skilltext>{props.description}</Skilltext>
+            </FlexWrapper>
         </StyledSkill>
     );
 };
 
 const StyledSkill = styled.div`
-  width: 30%;
-  background-color: rgba(255, 255, 255, 0.56);
-  margin: 10px;
+  width: 380px;
+  padding: 62px 20px 40px;
+`
+
+const IconWrapper = styled.div`
+  position: relative;
+  
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background: rgba(255, 255, 255, 0.10);
+    transform: rotate(45deg) translate(-50%, -50%);
+    
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform-origin: top left;
+  }
 `
 
 const SkillTitle = styled.h3`
-    
+    margin: 70px 0 15px ;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 1px;
+  text-transform: uppercase;
 `
 
 const Skilltext = styled.p`
-    
+    text-align: center;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.4;
 `
