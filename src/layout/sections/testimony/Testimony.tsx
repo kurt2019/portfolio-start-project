752fde074032dfd9420ca4366d8dc3ad1ebd9a86
kurt2019 <artemkurt@ukr.net>
2024-01-19 18:35:18 +0200

    Finish Testimony + Contact(p3)

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -5,26 +5,28 @@ import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {IconWrapper} from "../skills/skill/Skill";
+import {Container} from "../../../components/Container";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
-            <SectionTitle>Testimony</SectionTitle>
-            <FlexWrapper direction={"column"} align={"center"}>
-                <IconWrapper>
-                <Icon iconId={"quote"} />
-                </IconWrapper>
-                <Slider/>
-            </FlexWrapper>
-
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <IconWrapper>
+                        <Icon iconId={"quote"}/>
+                    </IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
         </StyledTestimony>
     );
 };
 
 const StyledTestimony = styled.section`
-    min-height: 50vh;
-    background-color: #aeb9ff;
-  
+  min-height: 50vh;
+  background-color: #aeb9ff;
+
   ${IconWrapper} {
     margin: 40px 0 72px;
   }
