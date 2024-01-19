1b49efe518b7ed95b6adfda5ede213d95f93394e
kurt2019 <artemkurt@ukr.net>
2024-01-19 19:06:19 +0200

    Finish Slogan + Footer(p3)

--- src/styles/Common.ts
+++ src/styles/Common.ts
@@ -13,6 +13,6 @@ export const font = ({family, weight, color, lineHeight, Fmin, Fmax}: FontPropsT
     font-family: ${family || "Poppins"};
     font-weight: ${weight || 400};
     color: ${color || theme.colors.font};
-    line-height:  ${lineHeight || 1.2}
+    line-height:  ${lineHeight || 1.2};
     font-size: calc( (100vw - 360px)/(1440 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
 `
