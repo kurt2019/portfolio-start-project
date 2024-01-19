a83c5c96ef6d9553a78f2abe27cf3d5abec20171
kurt2019 <artemkurt@ukr.net>
2024-01-19 18:25:28 +0200

    Finish Works(p3)

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -24,11 +24,14 @@ export const TabMenu = (props: { menuItems: Array<string> }) => {
 };
 
 const StyledTabMenu = styled.nav`
-  margin-bottom: 40px;
   ul {
     display: flex;
-    gap: 20px;
-    justify-content: center;
+    //gap: 20px;
+    justify-content: space-between;
+    max-width: 352px;
+    width: 100%;
+    border: 1px solid chartreuse;
+    margin: 0 auto 45px;
   }
 `
 const ListItem = styled.li`
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -18,6 +18,7 @@ export const Work = (props: WorkPropsType) => {
             <Image src={props.src} alt=""/>
                 <Button>view project</Button>
             </ImageWrapper>
+
             <Description>
                 <Title>{props.title}</Title>
                 <Text>{props.text}</Text>
@@ -30,8 +31,8 @@ export const Work = (props: WorkPropsType) => {
 
 const StyledWork = styled.div`
   background-color: ${theme.colors.secondaryBg};
-  max-width: 540px;
-  width: 100%;
+  width: 330px;
+  flex-grow: 1;
 
   ${Link} {
     padding: 10px 0;
@@ -40,21 +41,43 @@ const StyledWork = styled.div`
       margin-left: 20px;
     }
   }
+  
+  @media ${theme.media.desktop} {
+    max-width: 540px;
+  }
 `
 
 const ImageWrapper = styled.div`
   position: relative;
   
+  ${Button} {
+    opacity: 0;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    &::before {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    background: rgba(0, 0, 0, 0.30);
+    backdrop-filter: blur(4px);
+    opacity: 0;
+  }
+  
   &:hover {
     &::before {
-      content: "";
-      position: absolute;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      top: 0;
-      background: rgba(0, 0, 0, 0.30);
-      backdrop-filter: blur(4px);
+      opacity: 1;
     }
 
     ${Button} {
@@ -62,18 +85,15 @@ const ImageWrapper = styled.div`
     }
   }
 
-  ${Button} {
-    opacity: 0;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    
-    &::before {
-      width: 100%;
-      height: 100%;
+  @media ${theme.media.tablet} {
+      &::before {
+        opacity: 1;
+      }
+
+      ${Button} {
+        opacity: 1;
+      }
     }
-  }
   
 `
 
