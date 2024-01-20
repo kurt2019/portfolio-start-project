623cad4ee0c4a099e59957839ad29461289dd1af
kurt2019 <artemkurt@ukr.net>
2024-01-20 17:05:31 +0200

    Finish refactoring Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,21 +2,33 @@ import React from 'react';
 import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
-import {DesktopMenu} from "./desktopMenu/DesktopMenu";
-import {MobileMenu} from "./mobileMenu/ModileMenu";
+import {DesktopMenu} from "./headerMenu/desktopMenu/DesktopMenu";
+import {MobileMenu} from "./headerMenu/mobileMenu/ModileMenu";
 import {S} from "./Header_Styles"
 
 
 const items =["Home", "Skills", "Works", "Testimony", "Contact"]
 
 export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 768;
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth)
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+
     return (
         <S.Header>
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
             <Logo/>
-            <DesktopMenu menuItems={items}/>
-            <MobileMenu menuItems={items}/>
+
+            {width < breakpoint ? <MobileMenu menuItems={items}/>
+                                : <DesktopMenu menuItems={items}/>}
                 </FlexWrapper>
             </Container>
         </S.Header>
--- src/layout/header/headerMenu/HeaderMenu_Styles.ts
+++ src/layout/header/headerMenu/HeaderMenu_Styles.ts
@@ -1,30 +1,71 @@
-import React from 'react';
 import styled, {css} from "styled-components";
 import {theme} from "../../../styles/Theme";
-import {Menu} from "../menu/Menu";
 
+// Menu
 
-export const MobileMenu = (props: {menuItems: Array<string> }) => {
-    return (
-        <StyledMobileMenu>
-            <BurgerButton isOpen={false}>
-                <span></span>
-            </BurgerButton>
+const Link = styled.a`
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 400;
+  font-size: 30px;
+  text-align: center;
+  //color: transparent;
+`
 
-            <MobileMenuPopup isOpen={false}>
-                <Menu menuItems={props.menuItems}/>
-            </MobileMenuPopup>
-
-        </StyledMobileMenu>
-    );
-};
-
-const StyledMobileMenu = styled.nav`
-display: none;
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: inline-block;
+  height: 50%;
+  overflow-y: hidden;
+  //outline: 1px solid red;
+  color: ${theme.colors.accent};
   
-  @media ${theme.media.tablet} {
-    display: block;
+  & +& {
+    top: 50%;
+    
+    span {
+      display: inline-block;
+      transform: translateY(-50%);
+    }
   }
+`
+
+const MenuItem = styled.li`
+  position: relative;
+  
+  &::before {
+    content: "";
+    display: inline-block;
+    height: 3px;
+    background-color: ${theme.colors.accent};
+    
+    position: absolute;
+    top: 50%;
+    left: -10px;
+    right: -10px;
+    z-index: 1;
+    
+    transform: scale(0);
+  }
+  
+  &:hover {
+    &::before {
+      transform: scale(1);
+    }
+    ${Mask} {
+      transform: skewX(12deg) translateX(5px) ;
+      color: ${theme.colors.font};
+      & + ${Mask} {
+        transform: skewX(12deg) translateX(-5px);
+      }
+    }
+  }
+`
+
+// MobileMenu
+const MobileMenu = styled.nav`
+    
 `
 
 const MobileMenuPopup = styled.div<{isOpen: boolean}>`
@@ -51,7 +92,6 @@ const MobileMenuPopup = styled.div<{isOpen: boolean}>`
     flex-direction: column;
   }
 `
-
 
 const BurgerButton = styled.button<{isOpen: boolean}>`
   position: fixed;
@@ -106,4 +146,21 @@ const BurgerButton = styled.button<{isOpen: boolean}>`
   }
 `
 
+// DesktopMenu
+const DesktopMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 30px;
+    justify-content: center;
+  }
+`
 
+export const S = {
+    Link,
+    Mask,
+    MenuItem,
+    MobileMenu,
+    MobileMenuPopup,
+    BurgerButton,
+    DesktopMenu,
+}
