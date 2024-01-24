89acacf90f9ee6f3c59aa453a95b5bb5976ee709
kurt2019 <artemkurt@ukr.net>
2024-01-24 20:30:33 +0200

    Finish React-paralax-tilt (in Main)

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {S} from "./Main_Styles";
 import Typewriter from "typewriter-effect";
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
     return (
@@ -13,7 +14,6 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name>I am <span>Svetlana Dyablo</span></S.Name>
-                        {/*<S.MainTitle>A Web Developer.</S.MainTitle>*/}
                         <S.MainTitle>
                             <p>A Web Developer.</p>
                             <Typewriter
@@ -26,11 +26,19 @@ export const Main: React.FC = () => {
                             />
                         </S.MainTitle>
                     </div>
-
-                    <S.PhotoWrapper>
-                        <S.Photo src={photo} alt=""/>
-                    </S.PhotoWrapper>
-
+                    <Tilt
+                        className="parallax-effect-img"
+                        tiltMaxAngleX={40}
+                        tiltMaxAngleY={40}
+                        perspective={800}
+                        transitionSpeed={1500}
+                        scale={1.1}
+                        gyroscope={true}
+                    >
+                        <S.PhotoWrapper>
+                            <S.Photo src={photo} alt=""/>
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexWrapper>
             </Container>
         </S.Main>
