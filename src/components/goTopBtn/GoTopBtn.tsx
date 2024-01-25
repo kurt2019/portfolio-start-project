1112004d4f2cc201c1d497cae0b8732c9f105c1c
kurt2019 <artemkurt@ukr.net>
2024-01-25 21:43:33 +0200

    React-scroll GoTop button  (useEffect and addEventListener)

---
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import styled from "styled-components";
+import {Icon} from "../icon/Icon";
+import {animateScroll as scroll} from "react-scroll";
+
+export const GoTopBtn = () => {
+
+    const [showBtn, setshowBtn] = useState(false)
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200){
+                setshowBtn(true)
+            } else {
+                setshowBtn(false)
+            }
+        })
+    }, []);
+
+    return (
+        <>
+            {showBtn && (
+                <StyledGoTopBtn onClick={ () => {scroll.scrollToTop()}}>
+                    <Icon iconId={"arrowGoTop"} height={"15"} width={"16"} viewBox={"0 0 15 16"}/>
+                </StyledGoTopBtn>
+            )}
+        </>
+    );
+};
+
+const StyledGoTopBtn = styled.button`
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 0;
+  position: fixed;
+  right: 30px;
+  bottom: 30px;
+  width: 30px;
+  height: 30px;
+`
+
